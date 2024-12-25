1f7be27956d771346b6969e80341559425491d81
bmask2020 <bmask2020@gmail.com>
2024-12-25 12:08:51 +0200

    Show Brands & Products In App Home Page

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
    
     public function home() {
 
+        $brand = Brand::all();
+        $product = Product::all();
+
         return response()->json([
             'status'    => true,
             'message'   => 'App Home Page',
-            'data'      => []
+            'brand'     => $brand,
+            'product'   => $product
 
         ], 200);
 
