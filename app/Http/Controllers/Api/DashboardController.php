3d8b03d16f5257241eaf733a993d574b602ebf2a
bmask2020 <bmask2020@gmail.com>
2025-01-21 06:37:47 +0200

    Create Order & Purchase From API Part Four

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -590,7 +590,7 @@ class DashboardController extends Controller
                 'Content-Type: application/json',
                 'accept: application/json',
             ]);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, "\n{\n  \"method\": \"create\",\n  \"store\": 000000,\n  \"authkey\": \"0000000000\",\n  \"framed\": 1,\n  \"order\": {\n    \"cartid\": \"$user->id\",\n    \"test\": \"1\",\n    \"amount\": \"$orders_price\",\n    \"currency\": \"SAR\",\n    \"description\": \"My purchase\"\n  },\n  \"return\": {\n    \"authorised\": \"http://127.0.0.1:8000/api/authorised/$user->id\",\n    \"declined\": \"http://127.0.0.1:8000/api/declined/$user->id\",\n    \"cancelled\": \"http://127.0.0.1:8000/api/cancelled/$user->id\"\n  }\n}\n");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, "\n{\n  \"method\": \"create\",\n  \"store\": 000000,\n  \"authkey\": \"0000000\",\n  \"framed\": 1,\n  \"order\": {\n    \"cartid\": \"$user->id\",\n    \"test\": \"1\",\n    \"amount\": \"$orders_price\",\n    \"currency\": \"SAR\",\n    \"description\": \"My purchase\"\n  },\n  \"return\": {\n    \"authorised\": \"http://127.0.0.1:8000/api/authorised/$user->id\",\n    \"declined\": \"http://127.0.0.1:8000/api/declined/$user->id\",\n    \"cancelled\": \"http://127.0.0.1:8000/api/cancelled/$user->id\"\n  }\n}\n");
 
             $response = curl_exec($ch);
 
@@ -608,6 +608,8 @@ class DashboardController extends Controller
             ])->update([
                 'ref'   => $ref
             ]);
+
+            Cart::where('user_id', '=', $user->id)->delete();
 
             return response()->json([
 
@@ -644,7 +646,7 @@ class DashboardController extends Controller
             'Content-Type: application/json',
             'accept: application/json',
         ]);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "\n{\n  \"method\": \"check\",\n  \"store\": 0000000,\n  \"authkey\": \"00000000\",\n  \"order\": {\n    \"ref\": \"$orders->ref\"\n  }\n}\n");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "\n{\n  \"method\": \"check\",\n  \"store\": 00000,\n  \"authkey\": \"000000000\",\n  \"order\": {\n    \"ref\": \"$orders->ref\"\n  }\n}\n");
 
         $response = curl_exec($ch);
 
@@ -689,14 +691,38 @@ class DashboardController extends Controller
 
     public function declined($id) {
 
-        return $id;
+        Orders::where([
+
+            ['user_id', '=', $id],
+            ['status', '=', false]
+
+        ])->delete();
+
+        return response()->json([
+
+            'status'   => false,
+            'message'  => 'Your Payment Not Complete',
+
+        ], 200);
         
     } // End Method
 
 
     public function cancelled($id) {
 
-        return $id;
+        Orders::where([
+
+            ['user_id', '=', $id],
+            ['status', '=', false]
+
+        ])->delete();
+
+        return response()->json([
+
+            'status'   => false,
+            'message'  => 'Your Payment Not Complete',
+
+        ], 200);
         
     } // End Method
 
